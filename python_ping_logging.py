422708f086056b7c100e915928ec38925078e7ca
makingisfun <webmaster@makingisfun.de>
2019-05-08 13:14:03 +0200

    Updated Code

--- python_ping_logging.py
+++ python_ping_logging.py
@@ -2,10 +2,10 @@
 import pyping
 import time
 
-file = open(„pinglog.txt“,“a“)
-file.write(„MinPing,MaxPing \n“)
+file = open("pinglog.txt", "a")
+file.write("MinPing,MaxPing \n")
 
-r = pyping.ping(‚google.com‘)
+r = pyping.ping("google.com")
 # if ret_code == 0, the host is available
 # print(r.ret_code)
 maxping = r.max_rtt
@@ -13,5 +13,5 @@ minping = r.min_rtt
 #print(„MinPing = “ +  minping)
 #print(„MaxPing = “ +  maxping)
 #print(„—————-„)
-ping = minping + „,“ + maxping + „\n“
+ping = minping + "," + maxping + "\n"
 file.write(ping)
